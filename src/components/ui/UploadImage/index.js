f7d73f9c7fc5880a7d16ab6c564fbc6e40eaff3d
serg1999g <46674079+serg1999g@users.noreply.github.com>
2020-06-11 23:22:29 +0300

    Update upload image component

--- src/components/ui/UploadImage/index.js
+++ src/components/ui/UploadImage/index.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import * as PropTypes from 'prop-types';
+import classes from './UploadImage.module.scss';
+import SpriteIcon from "components/icons/SpriteIcon";
 
 
 const UploadImage = (
@@ -7,24 +9,34 @@ const UploadImage = (
         setFieldValue
     }
 ) => {
-    const [image, setImage] = useState([])
+    const [image, setImage] = useState('')
+
 
     const handleChange = (event) => {
         const file = event.currentTarget.files[0]
         setFieldValue('image', file)
         const reader = new FileReader();
         reader.onloadend = () => {
+
             setImage(reader.result);
         };
-        reader.readAsDataURL(file)
+        if (file) {
+            reader.readAsDataURL(file)
+        }
     }
 
 
     return (
-        <div><img src={image} alt=""/>
-            <div>
-                <input accept="image/*" type="file" onChange={handleChange}/>
-            </div>
+        <div className='d-flex justify-content-center'>
+            <label htmlFor="photo-upload" className={classes.customFileUpload}>
+                <div className={classes.imgWrap}>
+                    <img className={classes.image} htmlFor="photo-upload" src={image}/>
+                </div>
+                <SpriteIcon name='upload' className={classes.icon}/>
+                {!image ? <SpriteIcon className={classes.defaultIcon} name='user'/> : null}
+
+                <input className={classes.inputFile} id="photo-upload" accept="image/*" type="file" onChange={handleChange}/>
+            </label>
         </div>
 
     );
